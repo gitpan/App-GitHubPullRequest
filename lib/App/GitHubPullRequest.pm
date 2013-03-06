97c133a677ba635d9540171b1aa02868b6ee9466
Robin Smidsrød <robin@smidsrod.no>
2013-03-06 13:14:42 +0000

    Import of ROBINS/App-GitHubPullRequest-0.0.2 from CPAN.
    gitpan-cpan-distribution: App-GitHubPullRequest
    gitpan-cpan-version:      0.0.2
    gitpan-cpan-path:         ROBINS/App-GitHubPullRequest-0.0.2.tar.gz
    gitpan-cpan-author:       ROBINS
    gitpan-cpan-maturity:     released

--- lib/App/GitHubPullRequest.pm
+++ lib/App/GitHubPullRequest.pm
@@ -4,7 +4,7 @@ use feature qw(say);
 
 package App::GitHubPullRequest;
 {
-  $App::GitHubPullRequest::VERSION = '0.0.1';
+  $App::GitHubPullRequest::VERSION = '0.0.2';
 }
 
 # ABSTRACT: Command-line tool to query GitHub pull requests
@@ -17,6 +17,7 @@ sub new {
     return bless {}, $class;
 }
 
+
 sub run {
     my ($self, @args) = @_;
     my $cmd = scalar @args ? shift @args : 'list';
@@ -24,6 +25,7 @@ sub run {
     return $self->$method(@args) if $method;
     return $self->help(@args);
 }
+
 
 sub help {
     my ($self, @args) = @_;
@@ -42,15 +44,25 @@ EOM
     return 1;
 }
 
-sub patch {
-    my ($self, $number) = @_;
-    die("Please specify a pull request number.\n") unless $number;
-    my $patch = $self->_fetch_patch($number);
-    die("Unable to fetch patch for pull request $number.\n")
-        unless defined $patch;
-    print $patch;
+
+sub list {
+    my ($self, $state) = @_;
+    my $prs = $self->_fetch_all($state);
+    say ucfirst($prs->{'state'}) . " pull requests for '" . $prs->{"repo"} . "':";
+    unless ( $prs->{'pull_requests'} and @{ $prs->{'pull_requests'} } ) {
+        say "No pull requests found.";
+        return 0;
+    }
+    foreach my $pr ( @{ $prs->{"pull_requests"} } ) {
+        my $number = $pr->{"number"};
+        my $title = $pr->{"title"};
+        my $body = $pr->{"body"};
+        my $date = $pr->{"updated_at"} || $pr->{'created_at'};
+        say join(" ", $number, $date, $title);
+    }
     return 0;
 }
+
 
 sub show {
     my ($self, $number, @args) = @_;
@@ -82,21 +94,14 @@ sub show {
     return 0;
 }
 
-sub list {
-    my ($self, $state) = @_;
-    my $prs = $self->_fetch_all($state);
-    say ucfirst($prs->{'state'}) . " pull requests for '" . $prs->{"repo"} . "':";
-    unless ( $prs->{'pull_requests'} and @{ $prs->{'pull_requests'} } ) {
-        say "No pull requests found.";
-        return 0;
-    }
-    foreach my $pr ( @{ $prs->{"pull_requests"} } ) {
-        my $number = $pr->{"number"};
-        my $title = $pr->{"title"};
-        my $body = $pr->{"body"};
-        my $date = $pr->{"updated_at"} || $pr->{'created_at'};
-        say join(" ", $number, $date, $title);
-    }
+
+sub patch {
+    my ($self, $number) = @_;
+    die("Please specify a pull request number.\n") unless $number;
+    my $patch = $self->_fetch_patch($number);
+    die("Unable to fetch patch for pull request $number.\n")
+        unless defined $patch;
+    print $patch;
     return 0;
 }
 
@@ -207,13 +212,197 @@ __END__
 
 =pod
 
+=encoding utf-8
+
 =head1 NAME
 
 App::GitHubPullRequest - Command-line tool to query GitHub pull requests
 
 =head1 VERSION
 
-version 0.0.1
+version 0.0.2
+
+=head1 SYNOPSIS
+
+    $ pr
+    $ pr list closed # not shown by default
+    $ pr show 7      # also includes comments
+    $ pr patch 7     # can be piped to colordiff if you like colors
+    $ pr help
+
+=head1 INSTALLATION
+
+Install it by just typing in these few lines in your shell:
+
+    $ curl -L http://cpanmin.us | perl - --self-upgrade
+    $ cpanm App::GitHubPullRequest
+
+=head1 COMMANDS
+
+=head2 help
+
+Displays some help.
+
+=head2 list [<state>]
+
+Shows all pull requests in the given state. State can be either C<open> or
+C<closed>.  The default state is C<open>.  This is the default command if
+none is specified.
+
+=head2 show <number>
+
+Shows details about the specified pull request number. Also includes
+comments.
+
+=head2 patch <number>
+
+Shows the patch associated with the specified pull request number.
+
+=head1 METHODS
+
+=head2 run(@args)
+
+Calls any of the other listed public methods with specified arguments. This
+is usually called automatically when you invoke L<pr>.
+
+=head1 CAVEATS
+
+If you don't have C<git config github.user> and C<git config github.password>
+set in your git config, it will use unauthenticated API requests, which has
+a rate-limit of 60 requests. If you add your user + password info it should
+allow 5000 requests before you hit the limit.
+
+You must be standing in a directory that is a git dir and that directory must
+have a remote that points to github.com for the tool to work.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+L<pr>
+
+=back
+
+=head1 SEMANTIC VERSIONING
+
+This module uses semantic versioning concepts from L<http://semver.org/>.
+
+=for :stopwords cpan testmatrix url annocpan anno bugtracker rt cpants kwalitee diff irc mailto metadata placeholders metacpan
+
+=head1 SUPPORT
+
+=head2 Perldoc
+
+You can find documentation for this module with the perldoc command.
+
+  perldoc App::GitHubPullRequest
+
+=head2 Websites
+
+The following websites have more information about this module, and may be of help to you. As always,
+in addition to those websites please use your favorite search engine to discover more resources.
+
+=over 4
+
+=item *
+
+MetaCPAN
+
+A modern, open-source CPAN search engine, useful to view POD in HTML format.
+
+L<http://metacpan.org/release/App-GitHubPullRequest>
+
+=item *
+
+Search CPAN
+
+The default CPAN search engine, useful to view POD in HTML format.
+
+L<http://search.cpan.org/dist/App-GitHubPullRequest>
+
+=item *
+
+RT: CPAN's Bug Tracker
+
+The RT ( Request Tracker ) website is the default bug/issue tracking system for CPAN.
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-GitHubPullRequest>
+
+=item *
+
+AnnoCPAN
+
+The AnnoCPAN is a website that allows community annotations of Perl module documentation.
+
+L<http://annocpan.org/dist/App-GitHubPullRequest>
+
+=item *
+
+CPAN Ratings
+
+The CPAN Ratings is a website that allows community ratings and reviews of Perl modules.
+
+L<http://cpanratings.perl.org/d/App-GitHubPullRequest>
+
+=item *
+
+CPAN Forum
+
+The CPAN Forum is a web forum for discussing Perl modules.
+
+L<http://cpanforum.com/dist/App-GitHubPullRequest>
+
+=item *
+
+CPANTS
+
+The CPANTS is a website that analyzes the Kwalitee ( code metrics ) of a distribution.
+
+L<http://cpants.perl.org/dist/overview/App-GitHubPullRequest>
+
+=item *
+
+CPAN Testers
+
+The CPAN Testers is a network of smokers who run automated tests on uploaded CPAN distributions.
+
+L<http://www.cpantesters.org/distro/A/App-GitHubPullRequest>
+
+=item *
+
+CPAN Testers Matrix
+
+The CPAN Testers Matrix is a website that provides a visual overview of the test results for a distribution on various Perls/platforms.
+
+L<http://matrix.cpantesters.org/?dist=App-GitHubPullRequest>
+
+=item *
+
+CPAN Testers Dependencies
+
+The CPAN Testers Dependencies is a website that shows a chart of the test results of all dependencies for a distribution.
+
+L<http://deps.cpantesters.org/?module=App::GitHubPullRequest>
+
+=back
+
+=head2 Bugs / Feature Requests
+
+Please report any bugs or feature requests by email to C<bug-app-githubpullrequest at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-GitHubPullRequest>. You will be automatically notified of any
+progress on the request by the system.
+
+=head2 Source Code
+
+The code is open to the world, and available for you to hack on. Please feel free to browse it and play
+with it, or whatever. If you want to contribute patches, please send me a diff or prod me to pull
+from your repository :)
+
+L<http://github.com/robinsmidsrod/App-GitHubPullRequest>
+
+  git clone git://github.com/robinsmidsrod/App-GitHubPullRequest.git
 
 =head1 AUTHOR
 
